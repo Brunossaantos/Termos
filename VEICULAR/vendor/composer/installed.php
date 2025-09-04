d960f61bec385e434acaf3b8a38640101041cc18
Bruno Santos <brunossaantos@gmail.com>
2025-09-04 17:10:28 -0300

    Primeiro commit

--- VEICULAR/vendor/composer/installed.php
+++ VEICULAR/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => '__root__',
-        'pretty_version' => '1.0.0+no-version-set',
-        'version' => '1.0.0.0',
-        'reference' => null,
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '2867474bb38a4be7d7272e855c17099a2b36342e',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         '__root__' => array(
-            'pretty_version' => '1.0.0+no-version-set',
-            'version' => '1.0.0.0',
-            'reference' => null,
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '2867474bb38a4be7d7272e855c17099a2b36342e',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
